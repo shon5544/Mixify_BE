845cd28df3f2aa6d455953b315954a7bfac85972
shon5544 <sbs8239@gmail.com>
2023-04-28 01:07:27 +0900

    feat: Recipe 엔티티 필드, 연관관계 매핑 추가

--- src/main/java/devbeom/Mixify/domain/Recipe.java
+++ src/main/java/devbeom/Mixify/domain/Recipe.java
@@ -1,4 +1,55 @@
 package devbeom.Mixify.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Recipe {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    @Column(name = "like_cnt")
+    private int likeCnt;
+
+    @Column(name = "scrap_cnt")
+    private int scrapCnt;
+
+    @Column(name = "comment_cnt")
+    private int commentCnt;
+
+    private String thumbnail;
+
+    private float star;
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+    private final List<Step> steps = new ArrayList<>();
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.REMOVE)
+    private final List<Comment> commentList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "recipe", cascade = CascadeType.ALL)
+    private final List<Ingredient> ingredientList = new ArrayList<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Scrap scrap;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private HotRecipes hotRecipes;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private NewRecipes newRecipes;
 }
