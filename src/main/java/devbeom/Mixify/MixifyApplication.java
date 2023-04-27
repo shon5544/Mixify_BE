02e7974b3975f04a70903eea587978ccc9b7e3e5
shon5544 <sbs8239@gmail.com>
2023-04-27 23:18:18 +0900

    initial commit

---
+++ src/main/java/devbeom/Mixify/MixifyApplication.java
@@ -0,0 +1,13 @@
+package devbeom.Mixify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MixifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MixifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devbeom/Mixify/MixifyApplicationTests.java
@@ -0,0 +1,13 @@
+package devbeom.Mixify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MixifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
