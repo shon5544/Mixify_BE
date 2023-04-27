18e5d6748209c721df3f34bfbb918302226ebc34
shon5544 <sbs8239@gmail.com>
2023-04-28 01:14:33 +0900

    feat: Comment 엔티티 필드, 연관관계 매핑 추가

--- src/main/java/devbeom/Mixify/domain/Comment.java
+++ src/main/java/devbeom/Mixify/domain/Comment.java
@@ -1,4 +1,25 @@
 package devbeom.Mixify.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+    private float comment_star;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Recipe recipe;
 }
