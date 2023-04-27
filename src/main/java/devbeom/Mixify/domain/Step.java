d6e6fdc14ae83ab89a7e5e9a150d3c403c100ef2
shon5544 <sbs8239@gmail.com>
2023-04-28 01:33:45 +0900

    feat: Step 엔티티에 필드, 연관관계 매핑, 생성자 추가

--- src/main/java/devbeom/Mixify/domain/Step.java
+++ src/main/java/devbeom/Mixify/domain/Step.java
@@ -1,4 +1,33 @@
 package devbeom.Mixify.domain;
 
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Step {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int index;
+
+    private String content;
+    private String image;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Recipe recipe;
+
+    @Builder
+    public Step(int index, String content, String image, Recipe recipe) {
+        this.index = index;
+        this.content = content;
+        this.image = image;
+        this.recipe = recipe;
+    }
 }
