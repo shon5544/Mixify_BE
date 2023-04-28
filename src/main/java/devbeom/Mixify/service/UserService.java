eb0cf61118b9b38fcc1e048e7bf74d92e8a43710
shon5544 <sbs8239@gmail.com>
2023-04-28 16:06:59 +0900

    feat: UserService 기본 메서드 개발

---
+++ src/main/java/devbeom/Mixify/service/UserService.java
@@ -0,0 +1,26 @@
+package devbeom.Mixify.service;
+
+import devbeom.Mixify.domain.User;
+import devbeom.Mixify.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public void joinUser(User user) {
+        userRepository.save(user);
+    }
+
+    public User getUserById(Long userId) {
+        return userRepository.findById(userId)
+                .orElseThrow(EntityNotFoundException::new);
+    }
+}
