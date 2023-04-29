c013271fc150f331c1d97993c7b9337640f8e910
shon5544 <sbs8239@gmail.com>
2023-04-30 03:27:58 +0900

    refactor: 다형성 줄임. Controller, Service 직접 오고가는 DTO만 다형성을 적용하고 내부의 자잘한 DTO들은 다형성을 줄이는편이 더 좋겠다.

--- src/main/java/devbeom/Mixify/dto/controller/recipe/RecipeControllerGetRecipeDTO.java
+++ src/main/java/devbeom/Mixify/dto/controller/recipe/RecipeControllerGetRecipeDTO.java
@@ -2,13 +2,9 @@ package devbeom.Mixify.dto.controller.recipe;
 
 import devbeom.Mixify.domain.Recipe;
 import devbeom.Mixify.dto.controller.comment.CommentControllerForRecipeDTO;
-import devbeom.Mixify.dto.controller.comment.CommentControllerParentDTO;
 import devbeom.Mixify.dto.controller.ingredient.IngredientControllerForRecipeDTO;
-import devbeom.Mixify.dto.controller.ingredient.IngredientControllerParentDTO;
 import devbeom.Mixify.dto.controller.step.StepControllerForRecipeDTO;
-import devbeom.Mixify.dto.controller.step.StepControllerParentDTO;
 import devbeom.Mixify.dto.controller.user.UserControllerForRecipeDTO;
-import devbeom.Mixify.dto.controller.user.UserControllerParentDTO;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -27,11 +23,11 @@ public class RecipeControllerGetRecipeDTO extends RecipeControllerParentDTO{
 
     private float star;
 
-    private List<StepControllerParentDTO> stepList;
-    private List<CommentControllerParentDTO> commentList;
-    private List<IngredientControllerParentDTO> ingredientList;
+    private List<StepControllerForRecipeDTO> stepList;
+    private List<CommentControllerForRecipeDTO> commentList;
+    private List<IngredientControllerForRecipeDTO> ingredientList;
 
-    private final UserControllerParentDTO user = new UserControllerForRecipeDTO();
+    private final UserControllerForRecipeDTO user = new UserControllerForRecipeDTO();
 
     // 생성자로 바로 설정이 가능한 경우 생성자로도 DTO 초기화를 가능하게끔 했다.
     public RecipeControllerGetRecipeDTO(Recipe recipe) {
@@ -46,7 +42,7 @@ public class RecipeControllerGetRecipeDTO extends RecipeControllerParentDTO{
         this.commentCnt = recipe.getCommentCnt();
         this.star = recipe.getStar();
 
-        StepControllerParentDTO stepControllerParentDTO = new StepControllerForRecipeDTO();
+        StepControllerForRecipeDTO stepControllerParentDTO = new StepControllerForRecipeDTO();
         this.stepList = recipe.getSteps().stream()
                 .map(step -> {
                     stepControllerParentDTO.toDTO(step);
@@ -54,7 +50,7 @@ public class RecipeControllerGetRecipeDTO extends RecipeControllerParentDTO{
                 })
                 .collect(Collectors.toList());
 
-        CommentControllerParentDTO controllerParentDTO = new CommentControllerForRecipeDTO();
+        CommentControllerForRecipeDTO controllerParentDTO = new CommentControllerForRecipeDTO();
         this.commentList = recipe.getCommentList().stream()
                 .map(comment -> {
                     controllerParentDTO.toDTO(comment);
@@ -62,7 +58,7 @@ public class RecipeControllerGetRecipeDTO extends RecipeControllerParentDTO{
                 })
                 .collect(Collectors.toList());
 
-        IngredientControllerParentDTO ingredientControllerParentDTO = new IngredientControllerForRecipeDTO();
+        IngredientControllerForRecipeDTO ingredientControllerParentDTO = new IngredientControllerForRecipeDTO();
         this.ingredientList = recipe.getIngredientList().stream()
                 .map(ingredient -> {
                     ingredientControllerParentDTO.toDTO(ingredient);
