d04bdc626eda31a1d3c496fb9111a7aec272969a
shon5544 <sbs8239@gmail.com>
2023-04-29 19:39:13 +0900

    fix: RecipeService, ScrapService에 @Transactional 추가, 트랜잭션 적용함

--- src/main/java/devbeom/Mixify/service/RecipeService.java
+++ src/main/java/devbeom/Mixify/service/RecipeService.java
@@ -5,12 +5,15 @@ import devbeom.Mixify.repository.RecipeRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class RecipeService {
     private final RecipeRepository recipeRepository;
 
+    @Transactional
     public void createRecipe(Recipe recipe) {
         recipeRepository.save(recipe);
     }
