043b4a0685cb8ce86bd8960cc9ad8da5bec32d13
shon5544 <sbs8239@gmail.com>
2023-04-29 19:39:53 +0900

    feat: CommentService 추가

---
+++ src/main/java/devbeom/Mixify/service/CommentService.java
@@ -0,0 +1,19 @@
+package devbeom.Mixify.service;
+
+import devbeom.Mixify.domain.Comment;
+import devbeom.Mixify.repository.CommentRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class CommentService {
+    private final CommentRepository commentRepository;
+
+    @Transactional
+    public void createComment(Comment comment) {
+        commentRepository.save(comment);
+    }
+}
