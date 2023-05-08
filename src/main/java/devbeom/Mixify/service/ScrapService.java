32b7469806a0d1f344b315d83f2a3820dbf1cb54
shon5544 <sbs8239@gmail.com>
2023-05-09 01:29:56 +0900

    feat: getScrapList 메서드 추가 * Scrap 엔티티를 ScrapResDTO로 변환함

--- src/main/java/devbeom/Mixify/service/ScrapService.java
+++ src/main/java/devbeom/Mixify/service/ScrapService.java
@@ -1,10 +1,15 @@
 package devbeom.Mixify.service;
 
 import devbeom.Mixify.domain.Scrap;
+import devbeom.Mixify.dto.response.scrap.ScrapResDTO;
 import devbeom.Mixify.repository.ScrapRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -15,5 +20,13 @@ public class ScrapService {
     @Transactional
     public Scrap addScrap(Scrap scrap) {
         return scrapRepository.save(scrap);
+    }
+
+    public List<ScrapResDTO> getScrapList(Long userId) {
+        List<Scrap> scrapList = scrapRepository.findAllByUserId(userId).orElseThrow(EntityNotFoundException::new);
+
+        return scrapList.stream()
+                .map(ScrapResDTO::new)
+                .collect(Collectors.toList());
     }
 }
