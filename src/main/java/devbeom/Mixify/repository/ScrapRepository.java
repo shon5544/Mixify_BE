22521b730188b29c1e0d8cc6edc82475fdfd3294
shon5544 <sbs8239@gmail.com>
2023-05-09 01:29:05 +0900

    feat: findAllByUserId 메서드 추가. * Query를 작성하여 join fetch를 넣어 성능을 최적화 시켰음

--- src/main/java/devbeom/Mixify/repository/ScrapRepository.java
+++ src/main/java/devbeom/Mixify/repository/ScrapRepository.java
@@ -3,11 +3,22 @@ package devbeom.Mixify.repository;
 import devbeom.Mixify.domain.Scrap;
 import devbeom.Mixify.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ScrapRepository extends JpaRepository<Scrap, Long> {
     Optional<Scrap> findByUser(User user);
+
+    @Query("select s from Scrap s " +
+            "   join fetch s.user u " +
+            "   join fetch s.recipe r " +
+            "       join fetch r.user " +
+            "where u.id = :userId " +
+            "order by s.scrappedDate desc ")
+    Optional<List<Scrap>> findAllByUserId(@Param("userId") Long userId);
 }
