7bc356dc0b1440a63255c76b099c009be4f3d7d4
shon5544 <sbs8239@gmail.com>
2023-05-09 13:37:13 +0900

    refactor: validation 추가 및 에러 메시지 추가

--- src/main/java/devbeom/Mixify/dto/request/recipe/RecipeGeneralReqDTO.java
+++ src/main/java/devbeom/Mixify/dto/request/recipe/RecipeGeneralReqDTO.java
@@ -17,23 +17,28 @@ import java.util.List;
 @AllArgsConstructor
 public class RecipeGeneralReqDTO {
 
-    @NotBlank
+    @NotBlank(message = "제목은 반드시 들어가있어야합니다.")
     @Size(max = 50)
     private String title;
 
+    @NotBlank(message = "썸네일은 반드시 넣어주셔야 합니다.")
     private String thumbnail;
 
     // 아무래도 해당 클래스의 toEntity 메서드에 의존하는 것보단 직접 User를 가져와서 꽂아주는 게 제일 안전할 듯.
 //    private UserGeneralReqDTO user;
-    @NotNull
+    @NotNull(message = "userId에 대한 정보가 없습니다.")
     private Long userId;
 
     // 프론트 개발 편의성을 위해 연관관계 주인을 여기서 받음
     // DTO 받은 이후에 리스트에 있는 요소들을 엔티티로 바꾼 후 일일히 저장해주는 로직 필요.
+    @NotNull(message = "레시피의 단계는 누락되어선 안 됩니다.")
+    @Size(min = 1, message = "레시피의 단계는 적어도 한 개 이상이어야 합니다.")
     private List<StepGeneralReqDTO> steps;
 
     // 프론트 개발 편의성을 위해 연관관계 주인을 여기서 받음
     // DTO 받은 이후에 리스트에 있는 요소들을 엔티티로 바꾼 후 일일히 저장해주는 로직 필요.
+    @NotNull(message = "레시피의 재료는 누락되어선 안 됩니다.")
+    @Size(min = 1, message = "재료는 무조건 1개 이상이어야 합니다.")
     private List<IngredientGeneralReqDTO> ingredients;
 
     public Recipe toEntity(User user) {
